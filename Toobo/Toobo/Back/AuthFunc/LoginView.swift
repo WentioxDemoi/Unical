d00b880c108a239dbbf72801a6d9ec943d078ec3
Rémi Desbordes <remi.desbordes@epitech.eu>
2024-06-01 16:35:40 +0200

    [ADD] Login

--- Toobo/Toobo/Back/AuthFunc/LoginView.swift
+++ Toobo/Toobo/Back/AuthFunc/LoginView.swift
@@ -1,41 +1,117 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var username: String = ""
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var errorMessage: String?
+    @State private var isLogged: Bool = false // Ajoutez cet état
 
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Login")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.bottom, 40)
-
-            TextField("Email Address", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.emailAddress)
-                .autocapitalization(.none)
-                .padding(.horizontal, 20)
-
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 20)
-
-            Button(action: {
-                // Login action
-                print("Login button tapped. Email: \(email), Password: \(password)")
-            }) {
+        NavigationView {
+            VStack(spacing: 20) {
                 Text("Login")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 200, height: 50)
-                    .background(Color.blue)
-                    .cornerRadius(10)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 40)
+
+                TextField("email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.default)
+                    .autocapitalization(.none)
+                    .padding(.horizontal, 20)
+
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 20)
+
+                Button(action: handleLogin) {
+                    Text("Login")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding(.top, 20)
+
+                if let errorMessage = errorMessage {
+                    Text(errorMessage)
+                        .foregroundColor(.red)
+                        .padding(.top, 20)
+                }
+
+                NavigationLink(
+                    destination: MainPage(username: username).navigationBarHidden(true),
+                    isActive: $isLogged,
+                    label: { EmptyView() }
+                )
             }
-            .padding(.top, 20)
+            .padding()
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarHidden(true)
         }
-        .padding()
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+
+    private func handleLogin() {
+        let userLoginData = UserLogin(
+            email: email,
+            password: password
+        )
+
+        guard let url = URL(string: "http://localhost:8080/user/login") else {
+            print("Invalid URL")
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        do {
+            let jsonData = try JSONEncoder().encode(userLoginData)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user login data: \(error)")
+            return
+        }
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    self.errorMessage = error.localizedDescription
+                }
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                if let data = data {
+                    let errorMessage = String(data: data, encoding: .utf8) ?? "Unknown error"
+                    DispatchQueue.main.async {
+                        self.errorMessage = errorMessage
+                    }
+                }
+                return
+            }
+
+            if let data = data, let username = String(data: data, encoding: .utf8) {
+                DispatchQueue.main.async {
+                    print("User Logged In Successfully: \(username)")
+                    self.username = username // Mettez à jour la valeur du nom d'utilisateur local
+                }
+            } else {
+                print("Invalid response data")
+            }
+
+            DispatchQueue.main.async {
+                self.errorMessage = nil
+                self.isLogged = true // Changez l'état isLogged à true pour activer la NavigationLink
+            }
+        }.resume()
+
+
     }
 }
 
---
+++ Toobo/Toobo/Back/AuthFunc/UserLogin.swift
@@ -0,0 +1,13 @@
+//
+//  UserLogin.swift
+//  Toobo
+//
+//  Created by Rémi Desbordes on 01/06/2024.
+//
+
+import Foundation
+
+struct UserLogin: Codable {
+    let email: String
+    let password: String
+}
